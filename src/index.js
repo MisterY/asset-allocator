6b0f0924d51758ba84ac342d817f052e2123848e
Alen Siljak <alen.siljak@yahoo.com>
2017-05-08 22:11:43 +0200

    adding jquery

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
     The start page of the app.
 */
 
-console.log("alive");
+$(function() {
+    // display the file name on button click
+    $("#okButton").on('click', () => {
+        var fileName = $("#databaseForm").find("input[name='filename']").val();
+        console.log("selected file: ", fileName);
+    })
+});
